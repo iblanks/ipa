6abc8c797485c6d62f8e4b7fe06e771d798ed07d
iblanks <34507065+iblanks@users.noreply.github.com>
2019-05-01 15:21:26 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,9 @@
-hi~
+<?php
+header ("Content-type: text/html;charset=UTF-8"); //html/plain
+foreach ($_SERVER as $name => $value) {
+   if (substr($name, 0, 5) == 'HTTP_') { 
+	   $head .= "\r\n" . str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5))))) . ": " . $value; 
+   } 
+} 
+echo  urldecode("url: " . $_SERVER['REQUEST_URI'] . "\r\n" . "post: " . file_get_contents("php://input") . "\r\n" . $head);
+return;
